85122cbc08faccb87cfcfeb80706b7420abdfab5
mikhailmikhalkin <michail.michalkin@gmail.com>
2020-01-03 00:15:56 +0300

    update

--- Dockerfile
+++ Dockerfile
@@ -8,14 +8,6 @@ WORKDIR /app
 
 RUN pip install -r requirements.txt
 
-#RUN pip install pipenv
-
-
-#RUN pip install --no-cache-dir numpy scipy pandas matplotlib
-#RUN pipenv install --system --deploy --ignore-pipfile
-
-
-
 COPY . /app
 
 # Run server
--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
 from routes import app_routes
-
+import os
 
 
 app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000))
+
 app.register_blueprint(app_routes, root_path=app.instance_path)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=port)
 
